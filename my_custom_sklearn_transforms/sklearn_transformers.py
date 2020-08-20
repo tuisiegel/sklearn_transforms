2d719276570f5553b533e3f6e3191e32dcc7f8f8
tuisiegel <45953466+tuisiegel@users.noreply.github.com>
2020-08-20 20:26:03 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -13,20 +13,20 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
-    def imput(subject):
+    def imput(subject, data1):
         #subject = "GO"
         nota = "NOTA_" + subject
         nota
         reprovacoes = "REPROVACOES_" + subject
-        ap_null = data.loc[data[reprovacoes] == 0].loc[pd.isnull(data[nota])]
+        ap_null = data1.loc[data1[reprovacoes] == 0].loc[pd.isnull(data1[nota])]
         
-        rp_null = data.loc[data[reprovacoes] != 0].loc[pd.isnull(data[nota])] 
+        rp_null = data1.loc[data1[reprovacoes] != 0].loc[pd.isnull(data1[nota])] 
         
-        not_null = data.loc[pd.notnull(data[nota])]
+        not_null = data1.loc[pd.notnull(data1[nota])]
                 
-        ap_null[nota] = data[nota].mean()
+        ap_null[nota] = data1[nota].mean()
         
         rp_null[nota] = 0
         
-        data1 = pd.concat([ap_null, rp_null, not_null])
-        return data1
+        data2 = pd.concat([ap_null, rp_null, not_null])
+        return data2
