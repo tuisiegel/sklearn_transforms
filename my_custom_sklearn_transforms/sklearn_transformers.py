2b22a1ccb34c3679a1342c4f35baac0743ef36e1
tuisiegel <45953466+tuisiegel@users.noreply.github.com>
2020-08-20 19:26:37 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -8,13 +8,12 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
-    
     def imput(subject):
         #subject = "GO"
         nota = "NOTA_" + subject
