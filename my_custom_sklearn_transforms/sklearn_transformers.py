b0225ccf0e833ecba30aeb43e1cd7604ba2fd1ef
tuisiegel <45953466+tuisiegel@users.noreply.github.com>
2020-08-21 02:22:10 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+import pandas as pd
 class DropColumns(BaseEstimator, TransformerMixin):
     def __init__(self, columns):
         self.columns = columns
